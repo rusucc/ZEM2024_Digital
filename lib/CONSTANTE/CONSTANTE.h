aa78e458fbda790a58670a000c1623d2234c267c
Rusu Cosmin-Constantin <57033890+rusucc@users.noreply.github.com>
2024-04-25 08:58:55 +0300

    modified:   lib/CONSTANTE/CONSTANTE.h

--- lib/CONSTANTE/CONSTANTE.h
+++ lib/CONSTANTE/CONSTANTE.h
@@ -1,6 +1,6 @@
 #ifndef CONSTANTE_h
 #define CONSTANTE_h
-const int basePWM=38,KP = 3.2,KI = 0.009,KD = 3;
+const int basePWM=60,KP = 3.65,KI = 0.0071,KD = 4.7;
 const int turnPWM=40;
 const int forward_pwm = 40;
 
